6ee39a4f925f6300cbabf923384472f25495c8cc
Piotr Gryko <piotr.gryko@gmail.com>
2023-08-27 21:58:32 +0200

    Fix: added allowed hosts

--- server/server/settings.py
+++ server/server/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", "127.0.0.1,localhost", cast=lambda v: [s.strip() for s in v.split(",")])
 
 
 # Application definition
