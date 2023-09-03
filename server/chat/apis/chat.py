6e3d79dc0d044da5a75d55b98205e8835a6d14f6
Piotr Gryko <piotr.gryko@gmail.com>
2023-09-03 20:39:35 +0200

    Got replicate response working, but response is not streaming

--- server/chat/apis/chat.py
+++ server/chat/apis/chat.py
@@ -1,6 +1,6 @@
 from django.http import StreamingHttpResponse
 from ninja import Router
-
+from ninja.params import Form
 
 from structlog import get_logger
 
@@ -14,13 +14,14 @@ router = Router()
 
 
 # for Nginx proxy_buffering off;
-@router.post("stream_chat/{collection}")
+@router.post("stream_chat/{room_uuid}")
 async def stream_chat(
-    request, collection: UUID4, chat_input: str
+    request, room_uuid: UUID4, message: str = Form(...)
 ) -> StreamingHttpResponse:
     # Allow a user to have a chat using the data stored in a specific collection.
+
     response = StreamingHttpResponse(
-        streaming_content=services.get_replicate_stream(chat_input),
+        streaming_content=services.get_replicate_stream(message),
         content_type="text/event-stream",
     )
     response["Cache-Control"] = "no-cache"
