b76390f771fb65b05b0a07d12b1b20dc53cf055a
Piotr Gryko <piotr.gryko@gmail.com>
2023-09-13 21:50:11 +0200

    FEAT: added propper display of embeddings in admin interface

--- server/chat/admin.py
+++ server/chat/admin.py
@@ -9,7 +9,19 @@ from chat.singleton import ChromaDBSingleton
 
 @admin.register(ChromaDBCollection)
 class ChromaDBCollectionAdmin(admin.ModelAdmin):
-    list_display = ("chroma_id", "name", "count", "peek")
+    list_display = ("chroma_id", "name", "count")
+
+    readonly_fields = [f.name for f in ChromaDBCollection._meta.get_fields()]
+
+    def has_change_permission(self, request, obj=None):
+        # Return False to disable editing, but the change view will still be accessible
+        return (
+            False if obj else True
+        )  # This will allow adding new objects if needed, but won't allow editing existing ones
+
+    def has_delete_permission(self, request, obj=None):
+        # Return True to allow deletion. Adjust as necessary.
+        return True
 
     @transaction.atomic
     def get_queryset(self, request):
@@ -23,52 +35,76 @@ class ChromaDBCollectionAdmin(admin.ModelAdmin):
         # {'name': '66af9efe-1e99-40d2-9799-c1cf99d9f969',
         #  'id': UUID('02fa98e2-806c-4aa9-9913-b45cbd96e413'),
         #  'metadata': None}
-        ChromaDBCollection.objects.all().delete()
+        # Fetch existing rows based on ChromaDB IDs
+        existing_ids = {col.id: col for col in collections}
+        existing_rows = ChromaDBCollection.objects.filter(
+            chroma_id__in=existing_ids.keys()
+        )
 
-        instances = [
-            ChromaDBCollection(
-                chroma_id=col.id, name=col.name, count=col.count(), peek=col.peek()
+        to_update = []
+        to_create = []
+
+        # Check for updates or new rows
+        for row in existing_rows:
+            collection = existing_ids.pop(
+                row.chroma_id
+            )  # Remove from the dict to keep track of new vs. existing
+            row.name = collection.name
+            row.count = collection.count()
+            row.peek = collection.peek()
+            to_update.append(row)
+
+        # Any remaining collections in existing_ids dict are new and need to be created
+        for collection in existing_ids.values():
+            new_row = ChromaDBCollection(
+                chroma_id=collection.id,
+                name=collection.name,
+                count=collection.count(),
+                peek=collection.peek(),
             )
-            for col in collections
-        ]
+            to_create.append(new_row)
 
-        ChromaDBCollection.objects.bulk_create(instances)
+        # Perform bulk update and bulk create operations
+        if to_update:
+            ChromaDBCollection.objects.bulk_update(to_update, ["name", "count", "peek"])
+        if to_create:
+            ChromaDBCollection.objects.bulk_create(to_create)
+
+        # Delete rows that no longer exist in ChromaDB
+        ChromaDBCollection.objects.exclude(
+            chroma_id__in=[col.id for col in collections]
+        ).delete()
 
         return ChromaDBCollection.objects.all()
 
-    # def changelist_view(self, request, extra_context=None):
-    #     client = ChromaDBSingleton().get_client()
-    #     collections = client.list_collections()
-    #     # collections = client.peek()  # Get the first 10 items
-    #     extra_context = extra_context or {}
-    #     extra_context["collections"] = collections
-    #     return super().changelist_view(request, extra_context=extra_context)
-    #
-    # def count(self, obj):
-    #     client = ChromaDBSingleton().get_client()
-    #     return client.get_collection(name=obj.name).count()
-    #
-    # count.short_description = "Count"
+    def custom_delete_selected(self, request, queryset):
+        # Custom delete logic.
+        # Use the queryset to get the list of items selected in the admin
+        for obj in queryset:
+            # Your custom delete logic here. For example:
+            client = ChromaDBSingleton().get_client()
+            client.delete_collection(name=obj.name)
+            obj.delete()  # Optional: If you want to delete the item from the Django-managed table
 
-    def save_model(self, request, obj, form, change):
-        client = ChromaDBSingleton().get_client()
-        if change:
-            collection = client.get_collection(name=obj.name)
-            collection.modify(name=form.cleaned_data["name"])
-        else:
-            client.create_collection(name=form.cleaned_data["name"])
+        # Provide a success message
+        self.message_user(
+            request, "Selected collections have been successfully deleted."
+        )
+
+    custom_delete_selected.short_description = "Delete selected collections"
+
+    actions = [custom_delete_selected]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        # Remove the default delete_selected action
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
 
     def delete_model(self, request, obj):
         client = ChromaDBSingleton().get_client()
         client.delete_collection(name=obj.name)
-
-    def response_add(self, request, obj, post_url_continue=None):
-        messages.success(request, "Collection successfully created.")
-        return HttpResponseRedirect(request.path)
-
-    def response_change(self, request, obj):
-        messages.success(request, "Collection successfully modified.")
-        return HttpResponseRedirect(request.path)
 
     def response_delete(self, request, obj_display, obj_id):
         messages.success(request, "Collection successfully deleted.")
