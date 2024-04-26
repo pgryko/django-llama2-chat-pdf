54c0b952b6f48f07fbe9eaf91bdaa4c58224445d
Wojciech Zając <wojtek@reu.pl>
2024-04-26 11:23:56 +0200

    Updated llama model name

--- server/chat/services.py
+++ server/chat/services.py
@@ -80,7 +80,7 @@ async def get_replicate_stream(
         # The predict method returns an iterator, and you can iterate over that output.
         # Prompting guide https://replicate.com/blog/how-to-prompt-llama
         output: Iterator = replicate.run(
-            "replicate/llama-2-70b-chat:2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
+            "meta/llama-2-70b-chat",
             input={
                 "prompt": prompt,
                 "system_prompt": "",
